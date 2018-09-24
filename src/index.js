62e3f2f932433738dab5eff3ba153234930c86f8
saniok017 <saniok017@gmail.com>
2018-09-25 01:43:09 +0300

    quadratic-equation/4thTask: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  this.equation= equation.split(" ").join("");
+  this.answer = [];
+  this.a = parseInt(this.equation.slice(0,this.equation.indexOf("*")),10);
+  this.b = parseInt(this.equation.slice(this.equation.indexOf("*")+4,this.equation.lastIndexOf("*")),10);
+  this.c = parseInt(this.equation.slice(this.equation.lastIndexOf("*")+2),10);
+  this.result = (-1 * this.b + Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
+  this.result2 = (-1 * this.b - Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
+  this.answer.push(Math.round(this.result));
+  this.answer.push(Math.round(this.result2));
+  return this.answer.sort(function(a,b){return a-b})
 }
