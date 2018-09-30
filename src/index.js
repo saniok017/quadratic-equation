ce32889f528f22b86de4ccf37a23ffe5a3b58744
saniok017 <saniok017@gmail.com>
2018-10-01 00:47:53 +0300

    quadratic-equation/task: cancer was removed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function solveEquation(equation) {
-  this.equation= equation.split(" ").join("");
-  this.answer = [];
-  this.a = parseInt(this.equation.slice(0,this.equation.indexOf("*")),10);
-  this.b = parseInt(this.equation.slice(this.equation.indexOf("*")+4,this.equation.lastIndexOf("*")),10);
-  this.c = parseInt(this.equation.slice(this.equation.lastIndexOf("*")+2),10);
-  this.result = (-1 * this.b + Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
-  this.result2 = (-1 * this.b - Math.sqrt(Math.pow(this.b, 2) - (4 * this.a * this.c))) / (2 * this.a);
-  this.answer.push(Math.round(this.result));
-  this.answer.push(Math.round(this.result2));
-  return this.answer.sort(function(a,b){return a-b})
+  const joinedEquation = equation.split(" ").join("");
+  const answer = [];
+  const a = parseInt(joinedEquation.slice(0 , joinedEquation.indexOf("*")),10);
+  const b = parseInt(joinedEquation.slice(joinedEquation.indexOf("*") + 4 , joinedEquation.lastIndexOf("*")),10);
+  const c = parseInt(joinedEquation.slice(joinedEquation.lastIndexOf("*")+2),10);
+  const discriminant = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+  const discriminant2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+  answer.push(Math.round(discriminant));
+  answer.push(Math.round(discriminant2));
+  return answer.sort(function(a,b){return a-b})
 }
